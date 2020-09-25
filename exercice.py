7ff04393fd7a095f8ad5143bba129c6581f360f4
anianiab <71099681+anianiab@users.noreply.github.com>
2020-09-24 20:54:18 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,20 +4,29 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
-
+      return len(string) % 2 == 0
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+    	num_char = 0 
+		for c in string:
+    		num_char += 1 if c == 0 else 0
+	  return num_char
 
 
 def get_first_part_of_name(name):
-	return ""
+    	first_part = name.split('-')[0]
+		capitalized = first_part[0].upper() + first_part[1:]	
+	return "Bonjour, " + capitalized
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+    	animal_index = [random.randrange(0, len(animals))]
+		animal_word = animal[animal_index]
 
+		ajd_index = random.randrange(0, len(animals))
+etc
+	return f"Aujourd'hui, j'ai vu un [animal_world] s'emparer d'un panier [adj] plein de [fruits]. "
+	return ""
 
 if __name__ == "__main__":
 	spam = "Bonjour!"
